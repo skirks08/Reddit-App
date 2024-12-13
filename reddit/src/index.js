8dfe65fd2c5ec05f944c5c599b663abeebe770ab
Michael Skirka <skirkamp@hotmail.com>
2024-12-13 23:01:37 +0000

    removed provider

--- reddit/src/index.js
+++ reddit/src/index.js
@@ -10,11 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
