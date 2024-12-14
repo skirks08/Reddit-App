aef7c940bfb0c5e14b53bc9f28b96dfa8aae3871
Michael Skirka <skirkamp@hotmail.com>
2024-12-14 16:37:39 +0000

    updated postsSlice, removed content from actions js which was the fetch posts const and added it within postSlice

--- reddit/src/Slices/postsSlice.js
+++ reddit/src/Slices/postsSlice.js
@@ -1,12 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { fetchPosts } from '../Actions';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import fetchFromReddit from '../utilities/redditApi';
+
+export const fetchPosts = createAsyncThunk(
+    'posts/fetchPosts',
+    async (subreddit, { rejectWithValue }) => {
+        try {
+            const data = await fetchFromReddit(`/r/${subreddit}.json`);
+            return data.data.children.map((child) => child.data);
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState: {
         posts: [],
+        status: 'idle',
         searchTerm: '',
         selectedCategory: '',
+        error: null,
+        cache: {},
     },
     reducers: {
         fetchPosts: (state, action) => {
@@ -19,8 +34,25 @@ const postsSlice = createSlice({
             state.selectedCategory = action.payload;
         },
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchPosts.pending, (state) => {
+                state.status = 'loading';
+                state.error = null;
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.posts = action.payload;
+
+                state.cache[action.meta.arg] = action.payload;
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.payload || 'Something went wrong';
+            })
+    },
 });
 
-export const { fetchPosts, setSearchTerm } = postsSlice.actions;
+export const { setSearchTerm } = postsSlice.actions;
 
 export default postsSlice.reducer;
